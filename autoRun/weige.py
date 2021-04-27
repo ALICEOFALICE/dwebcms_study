88fad7c43adabc39c77d5553058102bcf58eadcd
桜火 <41146748+ALICEOFALICE@users.noreply.github.com>
2021-04-27 22:56:12 +0800

    自动运行程序

---
+++ autoRun/weige.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3.7
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2021 桜火, Inc. All Rights Reserved 
+#
+# @Time    : 2021/4/26 21:35
+# @Author  : 桜火
+# @Email   : xie@loli.fit
+# @File    : aba.py
+# @Software: PyCharm
+import json
+from vika import Vika
+import os
+json_file = open("/home/runner/work/_temp/_github_workflow/event.json",mode="r")
+json_file = json_file.read()
+vika_token = os.environ["VKTOKEN"]
+vika_url = os.environ["VKURL"]
+env = json.loads(json_file)
+vika = Vika(vika_token)
+# 通过 datasheetId 来指定要从哪张维格表操作数据。
+datasheet = vika.datasheet(vika_url, field_key="id")
+row = datasheet.records.create({
+  "fldp3Jyag9dcT": str(env["commits"][0]["id"]),
+  "fldIilczY4AtL": env["repository"]["name"],
+  "fld4CxF56Aiqg": env["repository"]["description"],
+  "fldcNKW9tOQob": env["commits"][0]["author"]["name"],
+  "fldGoBim2N4ru": env["commits"][0]["author"]["username"],
+  "fldoDfcbS2FZT": env["commits"][0]["author"]["email"],
+  "fldSMyuxmszuo": env["repository"]["language"],
+  "fldUJsyEaYrET": env["commits"][0]["message"],
+  "fldDPC0Cjm6W3": env["commits"][0]["timestamp"],
+  "fldhpQbZKKfaW": env["repository"]["master_branch"],
+  "fld3fYMdpSROJ": env["repository"]["html_url"],
+})
+print(row)
+print("运行成功")
